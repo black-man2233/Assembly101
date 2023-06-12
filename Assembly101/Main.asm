e6cd4f542224223e88ad6af44243530009a10364
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-06-12 12:52:16 +0200

    added some items

--- Assembly101/Main.asm
+++ Assembly101/Main.asm
@@ -2,18 +2,17 @@ stack segment para stack
 	db 64 dup ('_')
 stack ends
 data segment
-
 	string db 'Hello World, from Assembly!','$'
-
+;	include proc.inc
 data ends
 
 code segment
-	assume cs:code,ds:data,ss:stack
+	assume cs:code, ds:data, ss:stack
 	mov ax,data
 	mov ds,ax
 	mov ax,stack
 	mov ss,ax
-	
+
 	main proc
 	
 		call show_string
